6778f0f280e8315c9b5e2b13da8b564d06d742e9
Angelo Silva <amsilvac1@uce.edu.ec>
2025-11-21 19:45:51 -0500

    Taller 13: Componentes Vue

---
+++ src/components/Contador.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Universidad Central del Ecuador</h1>
+    <h2>Carrera de Computación</h2>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
--- src/views/ContadorView.vue
+++ src/views/ContadorView.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
-    <ContadorView />
+    <Contador />
+    <Contador />
   </div>
 </template>
 
 <script>
-import ContadorView from '../src/views/ContadorView.vue'
+import Contador from '../components/Contador.vue'
 export default {
   components: {
-    ContadorView,
+    Contador,
   },
 }
 </script>
